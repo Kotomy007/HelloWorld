e1eae70d44d50df41dc9546eea863eef6864da6c
Vlad Kuleshin <kotomyminecrafter2@gmail.com>
2023-11-10 13:24:18 +0300

    123

---
+++ src/HomeWork.java
@@ -0,0 +1,32 @@
+public class HomeWork {
+    public static void main(String[] args) {
+
+        char homeChar = 'T' ;
+        int homeInt = 31434 ;
+        long homeLong = 321895683 ;
+        byte homeByte = 3 ;
+        short homeShort = 789 ;
+        float homeFloat = 8.7894f ;
+        double homeDouble = 3.1415926 ;
+
+        System.out.println(homeChar);
+        System.out.println(homeInt);
+        System.out.println(homeLong);
+        System.out.println(homeByte);
+        System.out.println(homeShort);
+        System.out.println(homeFloat);
+        System.out.println(homeDouble);
+
+        System.out.println();
+
+        int x = 502 ;
+        int y = 632 ;
+        int z = 321 ;
+
+        int intResult = x+y/z ;
+
+        System.out.println(intResult) ;
+
+
+    }
+}
